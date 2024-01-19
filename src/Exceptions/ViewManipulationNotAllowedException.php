f09f4e561328a5882419cb0bfb901ab65b463ebe
fl0cke <fl0cke@users.noreply.github.com>
2024-01-19 12:53:58 +0000

    Fix styling

--- src/Exceptions/ViewManipulationNotAllowedException.php
+++ src/Exceptions/ViewManipulationNotAllowedException.php
@@ -8,7 +8,7 @@ class ViewManipulationNotAllowedException extends \Exception
      * @param  string  $modelClassName
      *                                  {@inheritDoc}
      */
-    public function __construct(string $functionName, string $modelClassName, int $code = 0, \Throwable $previous = null)
+    public function __construct(string $functionName, string $modelClassName, int $code = 0, ?\Throwable $previous = null)
     {
         $message = sprintf('Calling [%s] method on the [%s] AG Grid model representing a database view is not allowed.', $functionName, $modelClassName);
         parent::__construct($message, $code, $previous);
