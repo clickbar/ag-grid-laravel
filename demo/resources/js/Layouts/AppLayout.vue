cdfacdbec0759e22a2c5969d9b452032b9617864
Jan Becker <jb@clickbar.dev>
2024-01-19 11:41:26 +0100

    add nav to AppLayout.vue

--- demo/resources/js/Layouts/AppLayout.vue
+++ demo/resources/js/Layouts/AppLayout.vue
@@ -1,10 +1,15 @@
 <script setup lang="ts">
-import ApplicationLogo from '@/Components/ApplicationLogo.vue';
 import { Link } from '@inertiajs/vue3';
 </script>
 
 <template>
-    <div class="h-screen  bg-gray-100">
-      <slot></slot>
+    <div class="h-screen bg-gray-100 flex flex-col">
+      <nav class="p-4 flex gap-4 text-sm text-gray-800 font-medium">
+          <Link class="hover:underline">Basic Demo</Link>
+          <Link class="hover:underline" href="/view">View Demo</Link>
+      </nav>
+      <div class="flex-1">
+        <slot></slot>
+      </div>
     </div>
 </template>
